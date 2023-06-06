98a729866b598b790017490233f7a602902a88e9
Huararanga <lukas.zubal@gmail.com>
2023-06-06 12:30:45 +0200

    app scaffolded + renaming the sample

---
+++ src/features/Employees/employeesSlice.ts
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+import { fetchCount } from './employeesAPI';
+
+export interface CounterState {
+  value: number;
+  status: 'idle' | 'loading' | 'failed';
+}
+
+const initialState: CounterState = {
+  value: 0,
+  status: 'idle',
+};
+
+export const incrementAsync = createAsyncThunk(
+  'employees/fetchCount',
+  async (amount: number) => {
+    const response = await fetchCount(amount);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const employeesSlice = createSlice({
+  name: 'employees',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(incrementAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.value += action.payload;
+      })
+      .addCase(incrementAsync.rejected, (state) => {
+        state.status = 'failed';
+      });
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = employeesSlice.actions;
+
+export const selectCount = (state: RootState) => state.employees.value;
+
+export const incrementIfOdd =
+  (amount: number): AppThunk =>
+  (dispatch, getState) => {
+    const currentValue = selectCount(getState());
+    if (currentValue % 2 === 1) {
+      dispatch(incrementByAmount(amount));
+    }
+  };
+
+export default employeesSlice.reducer;
